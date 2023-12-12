79f84de7ab91a7619eaf60c32c12b3383ad68a0f
internetHacker1123 <dinozavrik9911@mail.ru>
2023-12-12 18:22:44 +0300

    peremennye pomenyal

--- pyCharm/Hard.py
+++ pyCharm/Hard.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == '__main__':
-    firstWord = input("Введите первое слово: ").lower()
-    secondWord = input("Введите второе слово: ").lower()
-    value = firstWord + secondWord
+    first_word = input("Введите первое слово: ").lower()
+    second_word = input("Введите второе слово: ").lower()
+    value = first_word + second_word
     result = ''
     for i, symb in enumerate(value):
         if value.count(symb) == 1:
